13a820b46fab89c8fed3280e89e14b09b4b1e3f9
Hafiz M Hassan <hafiz.m.hassan@hotmail.com>
2016-06-01 16:54:30 +0500

    List Separator Enum added

---
+++ src/main/java/com/hassan/downloader/commons/ListSeparator.java
@@ -0,0 +1,11 @@
+package com.hassan.downloader.commons;
+
+public enum ListSeparator {
+	COMMA(","),
+	;
+	
+	public final String val;
+	
+	private ListSeparator(String val) { this.val = val; }
+
+}
