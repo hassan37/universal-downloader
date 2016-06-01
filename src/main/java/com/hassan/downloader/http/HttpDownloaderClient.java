23de4cc815088f15f98268f018018ca16d816ea7
Hafiz M Hassan <hafiz.m.hassan@hotmail.com>
2016-06-01 18:47:32 +0500

    HTTP Downloader is added

--- src/main/java/com/hassan/downloader/http/HttpDownloaderClient.java
+++ src/main/java/com/hassan/downloader/http/HttpDownloaderClient.java
@@ -1,5 +1,34 @@
 package com.hassan.downloader.http;
 
-public abstract class HttpDownloaderClient {
+import com.hassan.downloader.pojos.AppResponse;
+import com.hassan.downloader.pojos.DownloadRequest;
+import com.hassan.downloader.processor.ProcessorClient.Downloader;
+
+public class HttpDownloaderClient implements Downloader {
+
+	final DownloadRequest req;
+
+	final AppResponse resp;
+	
+	public static Downloader getInstance(final DownloadRequest req, final AppResponse resp) {
+		return new HttpDownloaderClient(req, resp);
+	}
+
+	private HttpDownloaderClient(final DownloadRequest req, final AppResponse resp) {
+		this.req = req;
+		this.resp = resp;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void download(DownloadRequest req, AppResponse callbackResp) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
